37d8811dd95a4d577acc5b7b22cf201143bc3c19
branko-bit <brankomajsen@gmail.com>
2025-03-29 18:44:19 +0100

    Added counting sort algorithm

--- main.cpp
+++ main.cpp
@@ -2,7 +2,31 @@
 #include <vector>
 using namespace std;
 
-void countingSort(int*& A, int*& B, int najmanjseStevilo, int najvecjeStevilo, int trenutnaVelikost){}
+void countingSort(int*& A, int*& B, int najmanjseStevilo, int najvecjeStevilo, int trenutnaVelikost){
+    int velikostC = najvecjeStevilo - najmanjseStevilo + 1;
+    
+	int* C = new int[velikostC];
+	for (int i = 0; i < velikostC; i++) {
+		C[i] = 0;
+	}
+
+	for (int i = 0; i < trenutnaVelikost; i++) {
+		C[A[i]] = C[A[i]] + 1;
+	}
+
+	for (int i = 1; i < velikostC; i++) {
+		C[i] = C[i] + C[i - 1];
+	}
+
+	B = new int[trenutnaVelikost];
+	
+	for (int i = trenutnaVelikost - 1; i >= 0; i--) {
+		B[C[A[i]] - 1] = A[i] + najmanjseStevilo;
+		C[A[i]] = C[A[i]] - 1;
+	}
+
+	delete[] C;
+}
 
 void romanSort(int*& A, int*& B, int najmanjseStevilo, int najvecjeStevilo, int trenutnaVelikost){}
 
