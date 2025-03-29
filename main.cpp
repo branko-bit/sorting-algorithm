fc1896d23034dd52d2455ec4dd0bd240fc0112f2
branko-bit <brankomajsen@gmail.com>
2025-03-29 18:22:06 +0100

    Added function declarations and parameters needed for them... Included all necessary libraires

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <vector>
+using namespace std;
+
+void countingSort(int*& A, int*& B, int najmanjseStevilo, int najvecjeStevilo, int trenutnaVelikost){}
+
+void romanSort(int*& A, int*& B, int najmanjseStevilo, int najvecjeStevilo, int trenutnaVelikost){}
+
+bool Branje_Stevil(int*& A, const char s[], int& velikost, int& najmanjseStevilo, int& najvecjeStevilo){}
+
+void Izpis_Stevil(int* polje, unsigned int velikost){}
+
+int main(int argc, const char* argv[]){
+
+    return 0;
+}
