25ce4e8054326e548bba52c30f4c709a1b3f5a32
branko-bit <brankomajsen@gmail.com>
2025-03-29 18:58:38 +0100

    Added reading numbers from file option/function

--- main.cpp
+++ main.cpp
@@ -66,7 +66,54 @@ void romanSort(int*& A, int*& B, int najmanjseStevilo, int najvecjeStevilo, int 
 	delete[] C;
 }
 
-bool Branje_Stevil(int*& A, const char s[], int& velikost, int& najmanjseStevilo, int& najvecjeStevilo){}
+bool Branje_Stevil(int*& A, const char s[], int& velikost, int& najmanjseStevilo, int& najvecjeStevilo){
+    ifstream input(s);
+	int st;
+	int trenutnaVelikost = 0;
+	int maxVelikost = 100;
+	A = new int[maxVelikost];
+
+	najmanjseStevilo = INT_MAX;
+	najvecjeStevilo = INT_MIN;
+	
+
+	if (!input.is_open()) {
+		return false;
+	}
+
+	while (!input.eof()) {
+		input >> st;
+		A[trenutnaVelikost] = st;
+		trenutnaVelikost++;
+
+		//dobi najmanjse stevilo
+		if (st < najmanjseStevilo) {
+			najmanjseStevilo = st;
+		}
+
+		//dobi najvecje steilo
+		if (st > najvecjeStevilo) {
+			najvecjeStevilo = st;
+		}
+
+		//povecuje veilkost A ce je premajhen
+		if (trenutnaVelikost == maxVelikost) {
+			maxVelikost *= 2;
+			int* povecanoPolje = new int[maxVelikost];
+
+			for (int i = 0; i < trenutnaVelikost; i++) {
+				povecanoPolje[i] = A[i];
+			}
+
+			delete[] A;
+			A = povecanoPolje;
+		}
+		while (isspace(input.peek())) input.get();
+	}
+	input.close();
+	velikost = trenutnaVelikost;
+	return true;
+}
 
 void Izpis_Stevil(int* polje, unsigned int velikost){
     ofstream output("out.txt");
@@ -76,6 +123,25 @@ void Izpis_Stevil(int* polje, unsigned int velikost){
 }
 
 int main(int argc, const char* argv[]){
+    int* A = nullptr;
+	int* B = nullptr;
+	int velikost = 0;
+	int najmanjseStevilo = INT_MAX;
+	int najvecjeStevilo = INT_MIN;
 
+	if (argc < 3) return 0;
+	if (!Branje_Stevil(A, argv[2], velikost, najmanjseStevilo, najvecjeStevilo)) return 0;
+
+	if (argv[1][0] == '0') {
+		//counting
+		countingSort(A, B, najmanjseStevilo, najvecjeStevilo, velikost);
+	}
+	else {
+		//Roman sort
+		romanSort(A, B, najmanjseStevilo, najvecjeStevilo, velikost);
+	}
+	Izpis_Stevil(B, velikost);
+
+	delete[] A;
     return 0;
 }
