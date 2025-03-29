3bd1c709f45cbd240c73a0919b22b91dbee5e31b
branko-bit <brankomajsen@gmail.com>
2025-03-29 18:51:35 +0100

    Added support for negative numbers to roman Sort algorithm

--- main.cpp
+++ main.cpp
@@ -36,6 +36,11 @@ void countingSort(int*& A, int*& B, int najmanjseStevilo, int najvecjeStevilo, i
 void romanSort(int*& A, int*& B, int najmanjseStevilo, int najvecjeStevilo, int trenutnaVelikost){
     int velikostC = najvecjeStevilo - najmanjseStevilo + 1;
 
+    //podpora negativnim
+	for (int i = 0; i < trenutnaVelikost; i++) {
+		A[i] = A[i] - najmanjseStevilo;
+	}
+
 	int* C = new int[velikostC];
 	for (int i = 0; i < velikostC; i++) {
 		C[i] = 0;
